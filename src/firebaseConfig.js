e774a9ba1eee05d289589938ec46fcc99a2aeb32
Carolina Gallarza <carolinagallarza2@gmail.com>
2023-08-12 17:20:27 -0300

    con API key

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -7,13 +7,14 @@ import {
 
 
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_APIKEY,
-    authDomain: import.meta.env.VITE_AUTH,
-    projectId: import.meta.env.VITE_PROJECTID,
-    storageBucket: import.meta.env.VITE_STORAGE,
-    messagingSenderId: import.meta.env.VITE_MESSAGING,
-    appId: import.meta.env.VITE_APP
+    apiKey: "AIzaSyCgHkvlC5M7N-Qf1098Eojur-GwXIBWJJ4",
+    authDomain: "primer-e-commerce-carol.firebaseapp.com",
+    projectId: "primer-e-commerce-carol",
+    storageBucket: "primer-e-commerce-carol.appspot.com",
+    messagingSenderId: "961140492355",
+    appId: "1:961140492355:web:4c5a2109ce4a8cfdebecf6"
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
